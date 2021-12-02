58ff4781517d5790b3bfcfcdc69872a9f03a9b26
dev4589 <86591336+dev4589@users.noreply.github.com>
2021-12-02 13:01:32 +0530

    new

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,6 +10,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  bool password = true;
   bool changeButton = false;
 
   final _formKey = GlobalKey<FormState>();
@@ -20,7 +21,8 @@ class _LoginPageState extends State<LoginPage> {
         changeButton = true;
       });
       await Future.delayed(Duration(seconds: 1));
-      await Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+      await Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
       setState(() {
         changeButton = false;
       });
@@ -34,97 +36,109 @@ class _LoginPageState extends State<LoginPage> {
         child: SingleChildScrollView(
           child: Form(
             key: _formKey,
-            child: Column(
-              children: [
-                Image.asset(
-                  "assets/logo.png",
-                  fit: BoxFit.cover,
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Text(
-                  "Welcome $name",
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: kPrimaryColor
+
+            child: Padding(
+              padding: const EdgeInsets.only(top: 84.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    "assets/logo.png",
+                    height: 200.0,
+                    width: 300.0,
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 32.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Enter username",
-                          labelText: "Username",
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Username cannot be empty";
-                          }
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "Welcome $name",
+                    style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: kPrimaryColor
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16.0, horizontal: 32.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "Enter username",
+                            labelText: "Username",
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Username cannot be empty";
+                            }
 
-                          return null;
-                        },
-                        onChanged: (value) {
-                          name = value;
-                          setState(() {});
-                        },
-                      ),
-                      TextFormField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: "Enter password",
-                          labelText: "Password",
+                            return null;
+                          },
+                          onChanged: (value) {
+                            name = value;
+                            setState(() {});
+                          },
                         ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Password cannot be empty";
-                          } else if (value.length < 6) {
-                            return "Password length should be atleast 6";
-                          }
-
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: 40.0,
-                      ),
-                      Material(
-                        color: kPrimaryColor,
-                        borderRadius:
-                            BorderRadius.circular(changeButton ? 50 : 8),
-                        child: InkWell(
-                          onTap: () => moveToHome(context),
-                          child: AnimatedContainer(
-                            duration: Duration(seconds: 1),
-                            width: changeButton ? 50 : 150,
-                            height: 50,
-                            alignment: Alignment.center,
-                            child: changeButton
-                                ? Icon(
-                                    Icons.done,
-                                    color: Colors.white,
+                        TextFormField(
+                          obscureText: password,
+                          decoration: InputDecoration(
+                              hintText: "Enter password",
+                              labelText: "Password",
+                              suffixIcon: IconButton(icon: Icon(Icons.lock),
+                                  onPressed: () {
+                                    setState(() {
+                                      password = !password;
+                                    });
+                                  },
                                   )
-                                : Text(
-                                    "Login",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18),
-                                  ),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Password cannot be empty";
+                            } else if (value.length < 6) {
+                              return "Password length should be atleast 6";
+                            }
+
+                            return null;
+                          },
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        Material(
+                          color: kPrimaryColor,
+                          borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8),
+                          child: InkWell(
+                            onTap: () => moveToHome(context),
+                            child: AnimatedContainer(
+                              duration: Duration(seconds: 1),
+                              width: changeButton ? 50 : 150,
+                              height: 50,
+                              alignment: Alignment.center,
+                              child: changeButton
+                                  ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                                  : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ));
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:project/splash_screen.dart';
 void main() {
 //  WidgetsFlutterBinding.ensureInitialized();
   //await Firebase.initializeApp();
+  Color kPrimaryColor = Color.fromRGBO(55, 105, 151, 1);
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
